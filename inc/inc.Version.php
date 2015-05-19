778e867618689ba92772e832fbac05467006cff5
Uwe Steinmann <steinm@debian.org>
2015-05-19 08:24:09 +0200

    new version 5.0.0

--- inc/inc.Version.php
+++ inc/inc.Version.php
@@ -20,7 +20,7 @@
 
 class SeedDMS_Version {
 
-	public $_number = "5.0.0-pre3";
+	public $_number = "5.0.0";
 	private $_string = "SeedDMS";
 
 	function SeedDMS_Version() {
--- install/install.php
+++ install/install.php
@@ -119,7 +119,7 @@ function fileExistsInIncludePath($file) { /* {{{ */
  * Load default settings + set
  */
 define("SEEDDMS_INSTALL", "on");
-define("SEEDDMS_VERSION", "5.0.0-pre3");
+define("SEEDDMS_VERSION", "5.0.0");
 
 require_once('../inc/inc.ClassSettings.php');
 
