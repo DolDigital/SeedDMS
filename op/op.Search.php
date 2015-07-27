572927668ab666a9da2fc8dda5db9f2d79831ebf
Uwe Steinmann <steinm@debian.org>
2015-07-27 20:54:21 +0200

    remove space in content type for excel sheet

--- op/op.Search.php
+++ op/op.Search.php
@@ -435,7 +435,7 @@ if(isset($_GET["fullsearch"]) && $_GET["fullsearch"]) {
 			header("Content-Transfer-Encoding: binary");
 			header("Content-Length: " . filesize($filename));
 			header("Content-Disposition: attachment; filename=\"export-" .date('Y-m-d') . ".xls\"");
-			header("Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml. sheet");
+			header("Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
 			header("Cache-Control: must-revalidate");
 		}
 
