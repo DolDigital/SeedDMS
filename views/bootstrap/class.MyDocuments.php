5ffbfbb898e75384ac0725a089db6c16f78ab655
Uwe Steinmann <steinm@debian.org>
2016-01-27 17:11:29 +0100

    list all docs in workflow owned by me and required operation by me

--- views/bootstrap/class.MyDocuments.php
+++ views/bootstrap/class.MyDocuments.php
@@ -396,6 +396,238 @@ class SeedDMS_View_MyDocuments extends SeedDMS_Bootstrap_Style {
 				else printMLText("no_docs_to_look_at");
 				
 				$this->contentContainerEnd();
+			} elseif($workflowmode == 'advanced') {
+				// Get document list for the current user.
+				$workflowStatus = $user->getWorkflowStatus();
+
+				// Create a comma separated list of all the documentIDs whose information is
+				// required.
+				$dList = array();
+				foreach ($workflowStatus["u"] as $st) {
+					if (!in_array($st["document"], $dList)) {
+						$dList[] = $st["document"];
+					}
+				}
+				foreach ($workflowStatus["g"] as $st) {
+					if (!in_array($st["document"], $dList)) {
+						$dList[] = $st["document"];
+					}
+				}
+				$docCSV = "";
+				foreach ($dList as $d) {
+					$docCSV .= (strlen($docCSV)==0 ? "" : ", ")."'".$d."'";
+				}
+				
+				if (strlen($docCSV)>0) {
+					// Get the document information.
+					$queryStr = "SELECT `tblDocuments`.*, `tblDocumentLocks`.`userID` as `lockUser`, ".
+						"`tblDocumentContent`.`version`, `tblDocumentStatus`.*, `tblDocumentStatusLog`.`status`, ".
+						"`tblDocumentStatusLog`.`comment` AS `statusComment`, `tblDocumentStatusLog`.`date` as `statusDate`, ".
+						"`tblDocumentStatusLog`.`userID`, `oTbl`.`fullName` AS `ownerName`, `sTbl`.`fullName` AS `statusName` ".
+						"FROM `tblDocumentContent` ".
+						"LEFT JOIN `tblDocuments` ON `tblDocuments`.`id` = `tblDocumentContent`.`document` ".
+						"LEFT JOIN `tblDocumentStatus` ON `tblDocumentStatus`.`documentID` = `tblDocumentContent`.`document` ".
+						"LEFT JOIN `tblDocumentStatusLog` ON `tblDocumentStatusLog`.`statusID` = `tblDocumentStatus`.`statusID` ".
+						"LEFT JOIN `ttstatid` ON `ttstatid`.`maxLogID` = `tblDocumentStatusLog`.`statusLogID` ".
+						"LEFT JOIN `ttcontentid` ON `ttcontentid`.`maxVersion` = `tblDocumentStatus`.`version` AND `ttcontentid`.`document` = `tblDocumentStatus`.`documentID` ".
+						"LEFT JOIN `tblDocumentLocks` ON `tblDocuments`.`id`=`tblDocumentLocks`.`document` ".
+						"LEFT JOIN `tblUsers` AS `oTbl` on `oTbl`.`id` = `tblDocuments`.`owner` ".
+						"LEFT JOIN `tblUsers` AS `sTbl` on `sTbl`.`id` = `tblDocumentStatusLog`.`userID` ".
+						"WHERE `ttstatid`.`maxLogID`=`tblDocumentStatusLog`.`statusLogID` ".
+						"AND `ttcontentid`.`maxVersion` = `tblDocumentContent`.`version` ".
+						"AND `tblDocumentStatusLog`.`status` IN (".S_IN_WORKFLOW.", ".S_EXPIRED.") ".
+						"AND `tblDocuments`.`id` IN (" . $docCSV . ") ".
+						"ORDER BY `statusDate` DESC";
+
+					$resArr = $db->getResultArray($queryStr);
+					if (is_bool($resArr) && !$resArr) {
+						$this->contentHeading(getMLText("warning"));
+						$this->contentContainer(getMLText("internal_error_exit"));
+						$this->htmlEndPage();
+						exit;
+					}
+					
+					// Create an array to hold all of these results, and index the array by
+					// document id. This makes it easier to retrieve document ID information
+					// later on and saves us having to repeatedly poll the database every time
+					// new document information is required.
+					$docIdx = array();
+					foreach ($resArr as $res) {
+						
+						// verify expiry
+						if ( $res["expires"] && time()>$res["expires"]+24*60*60 ){
+							if  ( $res["status"]==S_IN_WORKFLOW ){
+								$res["status"]=S_EXPIRED;
+							}
+						}
+
+						$docIdx[$res["id"]][$res["version"]] = $res;
+					}
+
+					// List the documents where a review has been requested.
+					$this->contentHeading(getMLText("documents_to_check"));
+					$this->contentContainerStart();
+					$printheader=true;
+					$iRev = array();
+					$dList = array();
+					foreach ($workflowStatus["u"] as $st) {
+					
+						if ( isset($docIdx[$st["document"]][$st["version"]]) && !in_array($st["document"], $dList) ) {
+							$dList[] = $st["document"];
+							$document = $dms->getDocument($st["document"]);
+						
+							if ($printheader){
+								print "<table class=\"table table-condensed\">";
+								print "<thead>\n<tr>\n";
+								print "<th></th>\n";
+								print "<th>".getMLText("name")."</th>\n";
+								print "<th>".getMLText("owner")."</th>\n";
+								print "<th>".getMLText("version")."</th>\n";
+								print "<th>".getMLText("last_update")."</th>\n";
+								print "<th>".getMLText("expires")."</th>\n";
+								print "</tr>\n</thead>\n<tbody>\n";
+								$printheader=false;
+							}
+						
+							print "<tr>\n";
+							$latestContent = $document->getLatestContent();
+							$previewer->createPreview($latestContent);
+							print "<td><a href=\"../op/op.Download.php?documentid=".$st["document"]."&version=".$st["version"]."\">";
+							if($previewer->hasPreview($latestContent)) {
+								print "<img class=\"mimeicon\" width=\"".$previewwidth."\"src=\"../op/op.Preview.php?documentid=".$document->getID()."&version=".$latestContent->getVersion()."&width=".$previewwidth."\" title=\"".htmlspecialchars($latestContent->getMimeType())."\">";
+							} else {
+								print "<img class=\"mimeicon\" src=\"".$this->getMimeIcon($latestContent->getFileType())."\" title=\"".htmlspecialchars($latestContent->getMimeType())."\">";
+							}
+							print "</a></td>";
+							print "<td><a href=\"out.ViewDocument.php?documentid=".$st["document"]."&currenttab=workflow\">".htmlspecialchars($docIdx[$st["document"]][$st["version"]]["name"])."</a></td>";
+							print "<td>".htmlspecialchars($docIdx[$st["document"]][$st["version"]]["ownerName"])."</td>";
+							print "<td>".$st["version"]."</td>";
+							print "<td>".$st["date"]." ". htmlspecialchars($docIdx[$st["document"]][$st["version"]]["statusName"]) ."</td>";
+							print "<td".($docIdx[$st["document"]][$st["version"]]['status']!=S_EXPIRED?"":" class=\"warning\"").">".(!$docIdx[$st["document"]][$st["version"]]["expires"] ? "-":getReadableDate($docIdx[$st["document"]][$st["version"]]["expires"]))."</td>";				
+							print "</tr>\n";
+						}
+					}
+					foreach ($workflowStatus["g"] as $st) {
+					
+						if (!in_array($st["document"], $iRev) && isset($docIdx[$st["document"]][$st["version"]]) && !in_array($st["document"], $dList) /* && $docIdx[$st["documentID"]][$st["version"]]['owner'] != $user->getId() */) {
+							$dList[] = $st["document"];
+							$document = $dms->getDocument($st["document"]);
+
+							if ($printheader){
+								print "<table class=\"table table-condensed\">";
+								print "<thead>\n<tr>\n";
+								print "<th></th>\n";
+								print "<th>".getMLText("name")."</th>\n";
+								print "<th>".getMLText("owner")."</th>\n";
+								print "<th>".getMLText("version")."</th>\n";
+								print "<th>".getMLText("last_update")."</th>\n";
+								print "<th>".getMLText("expires")."</th>\n";
+								print "</tr>\n</thead>\n<tbody>\n";
+								$printheader=false;
+							}
+
+							print "<tr>\n";
+							$latestContent = $document->getLatestContent();
+							$previewer->createPreview($latestContent);
+							print "<td><a href=\"../op/op.Download.php?documentid=".$st["document"]."&version=".$st["version"]."\">";
+							if($previewer->hasPreview($latestContent)) {
+								print "<img class=\"mimeicon\" width=\"".$previewwidth."\"src=\"../op/op.Preview.php?documentid=".$document->getID()."&version=".$latestContent->getVersion()."&width=".$previewwidth."\" title=\"".htmlspecialchars($latestContent->getMimeType())."\">";
+							} else {
+								print "<img class=\"mimeicon\" src=\"".$this->getMimeIcon($latestContent->getFileType())."\" title=\"".htmlspecialchars($latestContent->getMimeType())."\">";
+							}
+							print "</a></td>";
+							print "<td><a href=\"out.ViewDocument.php?documentid=".$st["document"]."&currenttab=workflow\">".htmlspecialchars($docIdx[$st["document"]][$st["version"]]["name"])."</a></td>";
+							print "<td>".htmlspecialchars($docIdx[$st["document"]][$st["version"]]["ownerName"])."</td>";
+							print "<td>".$st["version"]."</td>";
+							print "<td>".$st["date"]." ". htmlspecialchars($docIdx[$st["document"]][$st["version"]]["statusName"])."</td>";
+							print "<td".($docIdx[$st["document"]][$st["version"]]['status']!=S_EXPIRED?"":" class=\"warning\"").">".(!$docIdx[$st["document"]][$st["version"]]["expires"] ? "-":getReadableDate($docIdx[$st["document"]][$st["version"]]["expires"]))."</td>";				
+							print "</tr>\n";
+						}
+					}
+					if (!$printheader){
+						echo "</tbody>\n</table>";
+					}else{
+						printMLText("no_docs_to_check");
+					}
+					$this->contentContainerEnd();
+				}
+
+				// Get list of documents owned by current user that are pending review or
+				// pending approval.
+				$queryStr = "SELECT `tblDocuments`.*, `tblDocumentLocks`.`userID` as `lockUser`, ".
+					"`tblDocumentContent`.`version`, `tblDocumentStatus`.*, `tblDocumentStatusLog`.`status`, ".
+					"`tblDocumentStatusLog`.`comment` AS `statusComment`, `tblDocumentStatusLog`.`date` as `statusDate`, ".
+					"`tblDocumentStatusLog`.`userID`, `oTbl`.`fullName` AS `ownerName`, `sTbl`.`fullName` AS `statusName` ".
+					"FROM `tblDocumentContent` ".
+					"LEFT JOIN `tblDocuments` ON `tblDocuments`.`id` = `tblDocumentContent`.`document` ".
+					"LEFT JOIN `tblDocumentStatus` ON `tblDocumentStatus`.`documentID` = `tblDocumentContent`.`document` ".
+					"LEFT JOIN `tblDocumentStatusLog` ON `tblDocumentStatusLog`.`statusID` = `tblDocumentStatus`.`statusID` ".
+					"LEFT JOIN `ttstatid` ON `ttstatid`.`maxLogID` = `tblDocumentStatusLog`.`statusLogID` ".
+					"LEFT JOIN `ttcontentid` ON `ttcontentid`.`maxVersion` = `tblDocumentStatus`.`version` AND `ttcontentid`.`document` = `tblDocumentStatus`.`documentID` ".
+					"LEFT JOIN `tblDocumentLocks` ON `tblDocuments`.`id`=`tblDocumentLocks`.`document` ".
+					"LEFT JOIN `tblUsers` AS `oTbl` on `oTbl`.`id` = `tblDocuments`.`owner` ".
+					"LEFT JOIN `tblUsers` AS `sTbl` on `sTbl`.`id` = `tblDocumentStatusLog`.`userID` ".
+					"WHERE `ttstatid`.`maxLogID`=`tblDocumentStatusLog`.`statusLogID` ".
+					"AND `ttcontentid`.`maxVersion` = `tblDocumentContent`.`version` ".
+					"AND `tblDocuments`.`owner` = '".$user->getID()."' ".
+					"AND `tblDocumentStatusLog`.`status` IN (".S_IN_WORKFLOW.") ".
+					"ORDER BY `statusDate` DESC";
+
+				$resArr = $db->getResultArray($queryStr);
+				if (is_bool($resArr) && !$resArr) {
+					$this->contentHeading(getMLText("warning"));
+					$this->contentContainer("Internal error. Unable to complete request. Exiting.");
+					$this->htmlEndPage();
+					exit;
+				}
+
+				$this->contentHeading(getMLText("documents_user_requiring_attention"));
+				$this->contentContainerStart();
+				if (count($resArr)>0) {
+
+					print "<table class=\"table table-condensed\">";
+					print "<thead>\n<tr>\n";
+					print "<th></th>";
+					print "<th>".getMLText("name")."</th>\n";
+					print "<th>".getMLText("status")."</th>\n";
+					print "<th>".getMLText("version")."</th>\n";
+					print "<th>".getMLText("last_update")."</th>\n";
+					print "<th>".getMLText("expires")."</th>\n";
+					print "</tr>\n</thead>\n<tbody>\n";
+
+					foreach ($resArr as $res) {
+						$document = $dms->getDocument($res["documentID"]);
+					
+						// verify expiry
+						if ( $res["expires"] && time()>$res["expires"]+24*60*60 ){
+							if  ( $res["status"]==S_DRAFT_APP || $res["status"]==S_DRAFT_REV ){
+								$res["status"]=S_EXPIRED;
+							}
+						}
+					
+						print "<tr>\n";
+						$latestContent = $document->getLatestContent();
+						$previewer->createPreview($latestContent);
+						print "<td><a href=\"../op/op.Download.php?documentid=".$res["documentID"]."&version=".$res["version"]."\">";
+						if($previewer->hasPreview($latestContent)) {
+							print "<img class=\"mimeicon\" width=\"".$previewwidth."\"src=\"../op/op.Preview.php?documentid=".$document->getID()."&version=".$latestContent->getVersion()."&width=".$previewwidth."\" title=\"".htmlspecialchars($latestContent->getMimeType())."\">";
+						} else {
+							print "<img class=\"mimeicon\" src=\"".$this->getMimeIcon($latestContent->getFileType())."\" title=\"".htmlspecialchars($latestContent->getMimeType())."\">";
+						}
+						print "</a></td>";
+						print "<td><a href=\"out.ViewDocument.php?documentid=".$res["documentID"]."&currenttab=revapp\">" . htmlspecialchars($res["name"]) . "</a></td>\n";
+						print "<td>".getOverallStatusText($res["status"])."</td>";
+						print "<td>".$res["version"]."</td>";
+						print "<td>".$res["statusDate"]." ".htmlspecialchars($res["statusName"])."</td>";
+						print "<td>".(!$res["expires"] ? "-":getReadableDate($res["expires"]))."</td>";				
+						print "</tr>\n";
+					}		
+					print "</tbody></table>";	
+					
+				}
+				else printMLText("no_docs_to_look_at");
+
+				$this->contentContainerEnd();
 			}
 			
 			// Get list of documents locked by current user 
