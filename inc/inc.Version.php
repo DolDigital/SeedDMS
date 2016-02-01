0c8768a09e661841516357c508cafb587818f0e8
Uwe Steinmann <steinm@debian.org>
2016-02-01 20:49:28 +0100

    new version 4.3.24

--- inc/inc.Version.php
+++ inc/inc.Version.php
@@ -20,7 +20,7 @@
 
 class SeedDMS_Version {
 
-	public $_number = "4.3.23";
+	public $_number = "4.3.24";
 	private $_string = "SeedDMS";
 
 	function SeedDMS_Version() {
--- install/install.php
+++ install/install.php
@@ -118,7 +118,7 @@ function fileExistsInIncludePath($file) { /* {{{ */
  * Load default settings + set
  */
 define("SEEDDMS_INSTALL", "on");
-define("SEEDDMS_VERSION", "4.3.23");
+define("SEEDDMS_VERSION", "4.3.24");
 
 require_once('../inc/inc.ClassSettings.php');
 
