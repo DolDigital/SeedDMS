43e6583db9103291d50b5e3702c5b00b61df8929
Uwe Steinmann <steinm@debian.org>
2016-02-03 14:45:26 +0100

    check if attribute def. has value set if multiple is set

--- op/op.AttributeMgr.php
+++ op/op.AttributeMgr.php
@@ -63,6 +63,9 @@ if ($action == "addattrdef") {
 	if($minvalues > $maxvalues) {
 		UI::exitError(getMLText("admin_tools"),getMLText("attrdef_min_greater_max"));
 	}
+	if($multiple && $valueset == '') {
+		UI::exitError(getMLText("admin_tools"),getMLText("attrdef_multiple_needs_valueset"));
+	}
 
 	$newAttrdef = $dms->addAttributeDefinition($name, $objtype, $type, $multiple, $minvalues, $maxvalues, $valueset, $regex);
 	if (!$newAttrdef) {
@@ -136,6 +139,9 @@ else if ($action == "editattrdef") {
 	}
 	if($minvalues > $maxvalues) {
 		UI::exitError(getMLText("admin_tools"),getMLText("attrdef_min_greater_max"));
+	}
+	if($multiple && $valueset == '') {
+		UI::exitError(getMLText("admin_tools"),getMLText("attrdef_multiple_needs_valueset"));
 	}
 
 	if (!$attrdef->setName($name)) {
