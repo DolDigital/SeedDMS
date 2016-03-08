5471c17b0f568e5fc4f022da94b584c7d57116cb
Uwe Steinmann <steinm@debian.org>
2016-03-08 16:07:42 +0100

    better check if reviewer/approver still exist

--- views/bootstrap/class.ViewDocument.php
+++ views/bootstrap/class.ViewDocument.php
@@ -545,9 +545,9 @@ class SeedDMS_View_ViewDocument extends SeedDMS_Bootstrap_Style {
 						}
 						else {
 							$reqName = htmlspecialchars($required->getFullName()." (".$required->getLogin().")");
+							if($required->getId() == $user->getId() && ($user->getId() != $owner->getId() || $enableownerrevapp == 1))
+								$is_reviewer = true;
 						}
-						if($r["required"] == $user->getId() && ($user->getId() != $owner->getId() || $enableownerrevapp == 1))
-							$is_reviewer = true;
 						break;
 					case 1: // Reviewer is a group.
 						$required = $dms->getGroup($r["required"]);
@@ -614,9 +614,9 @@ class SeedDMS_View_ViewDocument extends SeedDMS_Bootstrap_Style {
 						}
 						else {
 							$reqName = htmlspecialchars($required->getFullName()." (".$required->getLogin().")");
+							if($required->getId() == $user->getId())
+								$is_approver = true;
 						}
-						if($a["required"] == $user->getId())
-							$is_approver = true;
 						break;
 					case 1: // Approver is a group.
 						$required = $dms->getGroup($a["required"]);
@@ -625,9 +625,9 @@ class SeedDMS_View_ViewDocument extends SeedDMS_Bootstrap_Style {
 						}
 						else {
 							$reqName = "<i>".htmlspecialchars($required->getName())."</i>";
+							if($required->isMember($user) && ($user->getId() != $owner->getId() || $enableownerrevapp == 1))
+								$is_approver = true;
 						}
-						if($required->isMember($user) && ($user->getId() != $owner->getId() || $enableownerrevapp == 1))
-							$is_approver = true;
 						break;
 				}
 				print "<tr>\n";
