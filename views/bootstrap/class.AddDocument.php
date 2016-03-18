a7a9bc72a22f317cad5fd1559e3d69eda2aaabeb
Uwe Steinmann <steinm@debian.org>
2016-03-18 16:38:36 +0100

    get $libraryfolder from view in js()

--- views/bootstrap/class.AddDocument.php
+++ views/bootstrap/class.AddDocument.php
@@ -32,6 +32,7 @@ require_once("class.Bootstrap.php");
 class SeedDMS_View_AddDocument extends SeedDMS_Bootstrap_Style {
 
 	function js() { /* {{{ */
+		$libraryfolder = $this->params['libraryfolder'];
 		header('Content-Type: application/javascript; charset=UTF-8');
 ?>
 function checkForm()
