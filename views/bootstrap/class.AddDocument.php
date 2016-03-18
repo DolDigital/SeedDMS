513ed0c4e14dfba2c09e0e375dadcd7f2a90d641
Uwe Steinmann <steinm@debian.org>
2016-03-18 16:39:22 +0100

    get $dropfolderdir from view in js()

--- views/bootstrap/class.AddDocument.php
+++ views/bootstrap/class.AddDocument.php
@@ -32,6 +32,7 @@ require_once("class.Bootstrap.php");
 class SeedDMS_View_AddDocument extends SeedDMS_Bootstrap_Style {
 
 	function js() { /* {{{ */
+		$dropfolderdir = $this->params['dropfolderdir'];
 		header('Content-Type: application/javascript; charset=UTF-8');
 ?>
 function checkForm()
