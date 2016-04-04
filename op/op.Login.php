bc3450015a26b12e7feb5af467c4c36daaff0833
Uwe Steinmann <steinm@debian.org>
2016-04-04 09:00:08 +0200

    some code cleanup

--- op/op.Login.php
+++ op/op.Login.php
@@ -65,8 +65,6 @@ if($settings->_enableGuestLogin && (int) $settings->_guestID) {
 	}
 }
 
-$user = false;
-
 //
 // LDAP Sign In
 //
@@ -75,6 +73,8 @@ $user = false;
  * if authentication against ldap succeeds.
  * _ldapHost will only have a value if the ldap connector has been enabled
  */
+$user = false;
+
 if (!$user && isset($settings->_ldapHost) && strlen($settings->_ldapHost)>0) {
 	if (isset($settings->_ldapPort) && is_int($settings->_ldapPort)) {
 		$ds = ldap_connect($settings->_ldapHost, $settings->_ldapPort);
