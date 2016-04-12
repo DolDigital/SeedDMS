2d79a0f1439c46173b5d097783a2bb0860801d71
Uwe Steinmann <steinm@debian.org>
2016-04-12 17:05:05 +0200

    make php7 compatible

--- inc/inc.Version.php
+++ inc/inc.Version.php
@@ -23,8 +23,7 @@ class SeedDMS_Version {
 	public $_number = "4.3.25";
 	private $_string = "SeedDMS";
 
-	function SeedDMS_Version() {
-		return;
+	function __construct() {
 	}
 
 	function version() {
