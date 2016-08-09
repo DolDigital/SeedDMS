7c3e1c080797c8f0fe8fe9f4ea6b4af522930e85
Uwe Steinmann <steinm@debian.org>
2016-08-09 07:53:15 +0200

    fix copyright in header

--- out/out.WorkflowSummary.php
+++ out/out.WorkflowSummary.php
@@ -1,9 +1,9 @@
 <?php
 //    MyDMS. Document Management System
-//    Copyright (C) 2002-2005  Markus Westphal
+//    Copyright (C) 2002-2005 Markus Westphal
 //    Copyright (C) 2006-2008 Malcolm Cowe
 //    Copyright (C) 2010 Matteo Lucarelli
-//    Copyright (C) 2011-2013 Uwe Steinmann
+//    Copyright (C) 2010-2016 Uwe Steinmann
 //
 //    This program is free software; you can redistribute it and/or modify
 //    it under the terms of the GNU General Public License as published by
