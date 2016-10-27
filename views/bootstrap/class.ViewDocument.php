682b5dafe780f6596a5990b7a67265e3ff568fe0
Uwe Steinmann <steinm@debian.org>
2016-10-27 17:05:12 +0200

    do not check for enableOwnerRevApp anymore
    a user who has been once added as a reviewer/approver may have become
    the owner of the document later. Such a user may still be allowed
    to review/approve. enableOwnerRevApp will only affect the list of
    users when the reviewers/approvers are set.

--- views/bootstrap/class.ViewDocument.php
+++ views/bootstrap/class.ViewDocument.php
@@ -577,7 +577,7 @@ class SeedDMS_View_ViewDocument extends SeedDMS_Bootstrap_Style {
 						}
 						else {
 							$reqName = htmlspecialchars($required->getFullName()." (".$required->getLogin().")");
-							if($required->getId() == $user->getId() && ($user->getId() != $owner->getId() || $enableownerrevapp == 1))
+							if($required->getId() == $user->getId()/* && ($user->getId() != $owner->getId() || $enableownerrevapp == 1)*/)
 								$is_reviewer = true;
 						}
 						break;
@@ -588,7 +588,7 @@ class SeedDMS_View_ViewDocument extends SeedDMS_Bootstrap_Style {
 						}
 						else {
 							$reqName = "<i>".htmlspecialchars($required->getName())."</i>";
-							if($required->isMember($user) && ($user->getId() != $owner->getId() || $enableownerrevapp == 1))
+							if($required->isMember($user)/* && ($user->getId() != $owner->getId() || $enableownerrevapp == 1)*/)
 								$is_reviewer = true;
 						}
 						break;
@@ -657,7 +657,7 @@ class SeedDMS_View_ViewDocument extends SeedDMS_Bootstrap_Style {
 						}
 						else {
 							$reqName = "<i>".htmlspecialchars($required->getName())."</i>";
-							if($required->isMember($user) && ($user->getId() != $owner->getId() || $enableownerrevapp == 1))
+							if($required->isMember($user)/* && ($user->getId() != $owner->getId() || $enableownerrevapp == 1)*/)
 								$is_approver = true;
 						}
 						break;
