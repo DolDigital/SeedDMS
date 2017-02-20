5f9894b7b4a02a370d3bfb46994fa1d5941ba943
Uwe Steinmann <steinm@debian.org>
2017-02-20 09:12:19 +0100

    add db changes for version 5.1.0

---
+++ install/update-5.1.0/update.sql
@@ -0,0 +1,20 @@
+START TRANSACTION;
+
+ALTER TABLE `tblDocumentContent` CHANGE `mimeType` `mimeType` varchar(100) NOT NULL default '';
+
+ALTER TABLE `tblDocumentFiles` CHANGE `mimeType` `mimeType` varchar(100) NOT NULL default '';
+
+ALTER TABLE `tblUsers` CHANGE `pwdExpiration` `pwdExpiration` datetime default NULL;
+
+ALTER TABLE `tblUserPasswordRequest` CHANGE `date` `date` datetime NOT NULL;
+
+ALTER TABLE `tblUserPasswordHistory` CHANGE `date` `date` datetime NOT NULL;
+
+ALTER TABLE `tblDocumentApproveLog` CHANGE `date` `date` datetime NOT NULL;
+
+ALTER TABLE `tblDocumentReviewLog` CHANGE `date` `date` datetime NOT NULL;
+
+ALTER TABLE `tblDocumentStatusLog` CHANGE `date` `date` datetime NOT NULL;
+
+COMMIT;
+
