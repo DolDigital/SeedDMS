3de40aa6b17a85cd971ba9fbe6adb2c048279966
Uwe Steinmann <steinm@debian.org>
2017-05-12 08:14:24 +0200

    fix doc of init(), some globals are not available

--- ext/example/class.example.php
+++ ext/example/class.example.php
@@ -37,9 +37,6 @@ class SeedDMS_ExtExample extends SeedDMS_ExtBase {
 	 *
 	 * Use this method to do some initialization like setting up the hooks
 	 * You have access to the following global variables:
-	 * $GLOBALS['dms'] : object representing dms
-	 * $GLOBALS['user'] : currently logged in user
-	 * $GLOBALS['session'] : current session
 	 * $GLOBALS['settings'] : current global configuration
 	 * $GLOBALS['settings']['_extensions']['example'] : configuration of this extension
 	 * $GLOBALS['LANG'] : the language array with translations for all languages
