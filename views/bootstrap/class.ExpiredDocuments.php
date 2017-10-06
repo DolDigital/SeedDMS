0577a058dc472fb4c8388dcc77eb661c1b9f710e
Uwe Steinmann <steinm@debian.org>
2017-10-06 18:05:39 +0200

    fix title of page

--- views/bootstrap/class.ExpiredDocuments.php
+++ views/bootstrap/class.ExpiredDocuments.php
@@ -52,46 +52,46 @@ class SeedDMS_View_ExpiredDocuments extends SeedDMS_Bootstrap_Style {
 		$this->contentStart();
 		$this->pageNavigation(getMLText("expired_documents"), "admin_tools");
 
-			$this->contentHeading(getMLText("all_documents"));
-			$this->contentContainerStart();
+		$this->contentHeading(getMLText("expired_documents"));
+		$this->contentContainerStart();
 
-			if($docs = $dms->getDocumentsExpired(-1400)) {
+		if($docs = $dms->getDocumentsExpired(-1400)) {
 
-				print "<table class=\"table table-condensed\">";
-				print "<thead>\n<tr>\n";
-				print "<th></th>";
-				print "<th><a href=\"../out/out.ExpiredDocuments.php?orderby=n\">".getMLText("name")."</a></th>\n";
-				print "<th><a href=\"../out/out.ExpiredDocuments.php?orderby=s\">".getMLText("status")."</a></th>\n";
-				print "<th>".getMLText("version")."</th>\n";
+			print "<table class=\"table table-condensed\">";
+			print "<thead>\n<tr>\n";
+			print "<th></th>";
+			print "<th><a href=\"../out/out.ExpiredDocuments.php?orderby=n\">".getMLText("name")."</a></th>\n";
+			print "<th><a href=\"../out/out.ExpiredDocuments.php?orderby=s\">".getMLText("status")."</a></th>\n";
+			print "<th>".getMLText("version")."</th>\n";
 //				print "<th><a href=\"../out/out.ExpiredDocuments.php?orderby=u\">".getMLText("last_update")."</a></th>\n";
-				print "<th><a href=\"../out/out.ExpiredDocuments.php?orderby=e\">".getMLText("expires")."</a></th>\n";
-				print "</tr>\n</thead>\n<tbody>\n";
+			print "<th><a href=\"../out/out.ExpiredDocuments.php?orderby=e\">".getMLText("expires")."</a></th>\n";
+			print "</tr>\n</thead>\n<tbody>\n";
 
-				$previewer = new SeedDMS_Preview_Previewer($cachedir, $previewwidth, $timeout);
-				foreach ($docs as $document) {
-					print "<tr>\n";
-					$latestContent = $document->getLatestContent();
-					$previewer->createPreview($latestContent);
-					print "<td><a href=\"../op/op.Download.php?documentid=".$document->getID()."&version=".$latestContent->getVersion()."\">";
-					if($previewer->hasPreview($latestContent)) {
-						print "<img class=\"mimeicon\" width=\"".$previewwidth."\" src=\"../op/op.Preview.php?documentid=".$document->getID()."&version=".$latestContent->getVersion()."&width=".$previewwidth."\" title=\"".htmlspecialchars($latestContent->getMimeType())."\">";
-					} else {
-						print "<img class=\"mimeicon\" width=\"".$previewwidth."\" src=\"".$this->getMimeIcon($latestContent->getFileType())."\" title=\"".htmlspecialchars($latestContent->getMimeType())."\">";
-					}
-					print "</a></td>";
-					print "<td><a href=\"out.ViewDocument.php?documentid=".$document->getID()."\">" . htmlspecialchars($document->getName()) . "</a></td>\n";
-					$status = $latestContent->getStatus();
-					print "<td>".getOverallStatusText($status["status"])."</td>";
-					print "<td>".$latestContent->getVersion()."</td>";
-//					print "<td>".$status["statusDate"]." ". htmlspecialchars($status["statusName"])."</td>";
-					print "<td>".(!$document->getExpires() ? "-":getReadableDate($document->getExpires()))."</td>";				
-					print "</tr>\n";
+			$previewer = new SeedDMS_Preview_Previewer($cachedir, $previewwidth, $timeout);
+			foreach ($docs as $document) {
+				print "<tr>\n";
+				$latestContent = $document->getLatestContent();
+				$previewer->createPreview($latestContent);
+				print "<td><a href=\"../op/op.Download.php?documentid=".$document->getID()."&version=".$latestContent->getVersion()."\">";
+				if($previewer->hasPreview($latestContent)) {
+					print "<img class=\"mimeicon\" width=\"".$previewwidth."\" src=\"../op/op.Preview.php?documentid=".$document->getID()."&version=".$latestContent->getVersion()."&width=".$previewwidth."\" title=\"".htmlspecialchars($latestContent->getMimeType())."\">";
+				} else {
+					print "<img class=\"mimeicon\" width=\"".$previewwidth."\" src=\"".$this->getMimeIcon($latestContent->getFileType())."\" title=\"".htmlspecialchars($latestContent->getMimeType())."\">";
 				}
-				print "</tbody></table>";
+				print "</a></td>";
+				print "<td><a href=\"out.ViewDocument.php?documentid=".$document->getID()."\">" . htmlspecialchars($document->getName()) . "</a></td>\n";
+				$status = $latestContent->getStatus();
+				print "<td>".getOverallStatusText($status["status"])."</td>";
+				print "<td>".$latestContent->getVersion()."</td>";
+//					print "<td>".$status["statusDate"]." ". htmlspecialchars($status["statusName"])."</td>";
+				print "<td>".(!$document->getExpires() ? "-":getReadableDate($document->getExpires()))."</td>";				
+				print "</tr>\n";
 			}
-			else printMLText("empty_notify_list");
-			
-			$this->contentContainerEnd();
+			print "</tbody></table>";
+		}
+		else printMLText("empty_notify_list");
+		
+		$this->contentContainerEnd();
 
 		$this->contentEnd();
 		$this->htmlEndPage();
