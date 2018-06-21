60a71e2397c0a4c85eb5fc489d7c3654a14738b8
Uwe Steinmann <steinm@debian.org>
2018-06-21 17:55:51 +0200

    run passStrength after loading page with ajax

--- styles/bootstrap/application.js
+++ styles/bootstrap/application.js
@@ -381,6 +381,20 @@ $(document).ready( function() {
 				width: '100%',
 				templateResult: chzn_template_func
 			});
+			$(".pwd").passStrength({ /* {{{ */
+				url: "../op/op.Ajax.php",
+				onChange: function(data, target) {
+					pwsp = 100*data.score;
+					$('#'+target+' div.bar').width(pwsp+'%');
+					if(data.ok) {
+						$('#'+target+' div.bar').removeClass('bar-danger');
+						$('#'+target+' div.bar').addClass('bar-success');
+					} else {
+						$('#'+target+' div.bar').removeClass('bar-success');
+						$('#'+target+' div.bar').addClass('bar-danger');
+					}
+				}
+			}); /* }}} */
 		});
 	}); /* }}} */
 
